580bc902aa5e8b9134fc25f2111bf040b1d2b8ba
Kirill Medvedev <repkomail@gmail.com>
2023-04-29 22:16:20 +0200

    feat: add styles for all elements

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-// import keyboardEN from './keys';
+import keyboardEN from './keys.js';
 
 const BODY = document.body;
 const MAINPAGE = document.createElement('div');
 const KEYBOARD = document.createElement('div');
-const TITLE = document.createElement('h1'); // создаем заголовок
-const TEXTAREA = document.createElement('textarea'); // создаем поле для ввода
+const TITLE = document.createElement('h1');
+const TEXTAREA = document.createElement('textarea');
 const KEYBOARDDIV = document.createElement('div');
 
 const init = () => {
-  MAINPAGE.classList.add('container'); // присваиваем класс диву
+  MAINPAGE.classList.add('container');
   KEYBOARD.classList.add('keyboard-container');
   KEYBOARDDIV.classList.add('keyboard');
-  TITLE.innerHTML = 'Virtual Keyboard';
-  // вставляем див
+  TITLE.textContent = 'Virtual Keyboard';
   BODY.insertAdjacentElement('afterbegin', MAINPAGE);
   MAINPAGE.append(KEYBOARD);
-  KEYBOARD.append(TITLE); // вставляем заголовок
+  KEYBOARD.append(TITLE);
   KEYBOARD.append(TEXTAREA);
   KEYBOARD.append(KEYBOARDDIV);
 };
