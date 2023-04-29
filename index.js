052918b678f7568bcbd7e72f37d0a813dd6debac
Kirill Medvedev <repkomail@gmail.com>
2023-04-29 21:07:54 +0200

    feat: add array with keyboard

---
+++ index.js
@@ -0,0 +1,2 @@
+import keyboardEN from './keys';
+
---
+++ keys.js
@@ -0,0 +1,9 @@
+const keyboardEN = [
+  '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'backspace',
+  'Tab', 'KeyQ', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'Enter',
+  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'",
+  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', 'ArrowUp', 'Shift',
+  'Control', 'Alt', 'space', 'ArrowLeft', 'ArrowDown', 'ArrowRight',
+];
+
+export default keyboardEN;
