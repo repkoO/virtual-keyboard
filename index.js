6fed7e66a6c5ee40c3c97f4069e170f354a94d08
Kirill Medvedev <repkomail@gmail.com>
2023-04-29 21:48:44 +0200

    feat: create all elements in DOM

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-import keyboardEN from './keys';
+// import keyboardEN from './keys';
 
+const BODY = document.body;
+const MAINPAGE = document.createElement('div');
+const KEYBOARD = document.createElement('div');
+const TITLE = document.createElement('h1'); // создаем заголовок
+const TEXTAREA = document.createElement('textarea'); // создаем поле для ввода
+const KEYBOARDDIV = document.createElement('div');
+
+const init = () => {
+  MAINPAGE.classList.add('container'); // присваиваем класс диву
+  KEYBOARD.classList.add('keyboard-container');
+  KEYBOARDDIV.classList.add('keyboard');
+  TITLE.innerHTML = 'Virtual Keyboard';
+  // вставляем див
+  BODY.insertAdjacentElement('afterbegin', MAINPAGE);
+  MAINPAGE.append(KEYBOARD);
+  KEYBOARD.append(TITLE); // вставляем заголовок
+  KEYBOARD.append(TEXTAREA);
+  KEYBOARD.append(KEYBOARDDIV);
+};
+
+init();
