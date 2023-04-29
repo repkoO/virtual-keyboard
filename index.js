1c4075f7a3f3b0635c4535ff09f54c5c8b29a23b
Kirill Medvedev <repkomail@gmail.com>
2023-04-29 22:35:21 +0200

    feat: create function to insert all buttons

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const TITLE = document.createElement('h1');
 const TEXTAREA = document.createElement('textarea');
 const KEYBOARDDIV = document.createElement('div');
 
+// функция создания элементов на странице//
 const init = () => {
   MAINPAGE.classList.add('container');
   KEYBOARD.classList.add('keyboard-container');
@@ -20,3 +21,41 @@ const init = () => {
 };
 
 init();
+
+// функция создания клавиш
+const createKeys = (keys) => {
+  keys.forEach((key) => {
+    const button = document.createElement('button');
+    button.textContent = key;
+    KEYBOARDDIV.append(button);
+    switch (key) {
+      case 'backspace':
+        button.classList.add('image__button', 'wide');
+        break;
+      case 'Enter':
+        button.classList.add('image__button', 'wide');
+        break;
+      case 'Tab':
+        button.classList.add('image__button', 'wide');
+        break;
+      case 'Shift':
+        button.classList.add('image__button', 'wide');
+        break;
+      case 'Control':
+        button.classList.add('image__button', 'wide');
+        break;
+      case 'Alt':
+        button.classList.add('image__button', 'wide');
+        break;
+      default:
+        button.classList.add('image__button');
+    }
+    if (key === 'space') {
+      button.classList.add('extra__wide');
+    } else if (key === 'CapsLock') {
+      button.classList.add('caps__wide');
+    }
+  });
+};
+
+createKeys(keyboardEN);
